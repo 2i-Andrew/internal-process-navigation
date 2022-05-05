6f2ab54a748ca9118e0162f59d1e0b5706a7cf63
jandrewgordon2i <andrew.gordon@2itesting.com>
2022-05-05 14:21:34 +0100

    first commit

---
+++ src/webparts/internalProcessNavigation/components/IInternalProcessNavigationProps.ts
@@ -0,0 +1,7 @@
+export interface IInternalProcessNavigationProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/internalProcessNavigation/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IInternalProcessNavigationWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'InternalProcessNavigationWebPartStrings' {
+  const strings: IInternalProcessNavigationWebPartStrings;
+  export = strings;
+}
